de6e4b8c2bbe5f085779ecdd28394ef1f0d75912
samuelowusu22 <Gracegaga20@gmail>
2024-04-22 15:41:59 +0100

    another change

--- api/post/read_comments.php
+++ api/post/read_comments.php
@@ -3,7 +3,7 @@
 //Headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
-    
+
     //include database
     include_once '../../config/Database.php';
     include_once '../../models/Post.php';
@@ -11,3 +11,6 @@
     //Instantiate Db and Connect
     $database = new Database();
     $db = $database->connect();
+
+  // below are going to be the funtion for adding comments
+  // below are going to be the function for calculating comments
